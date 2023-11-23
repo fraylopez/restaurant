df308bac30fb18e2b3a9f32625282d72b5ab46f6
Victor Moreno <>
2023-11-23 12:06:38 +0100

    test: added  some performance tests

--- test/performance.test.js
+++ test/performance.test.js
@@ -11,30 +11,66 @@ describe("Performance tests", () => {
     { numGroups: 10000, groupSize: 2 },
     { numGroups: 100000, groupSize: 2 },
   ].forEach((testCase) => {
-    describe(`Direct allocation of ${testCase.numGroups} groups`, () => {
-      let restaurant;
-      let groups;
-      it(`Given a restaurant with ${testCase.numGroups} tables with ${testCase.groupSize} seats each`, () => {});
-      it(`And ${testCase.numGroups} groups of ${testCase.groupSize}`, () => {
-        const tables = Array.from(
-          { length: testCase.numGroups },
-          () => new Table(testCase.groupSize)
-        );
-        restaurant = new Restaurant(tables);
-        groups = Array.from(
-          { length: testCase.numGroups },
-          () => new CustomerGroup(testCase.groupSize)
-        );
+    describe(`Direct allocation`, () => {
+      describe(`${testCase.numGroups} groups`, () => {
+        let restaurant;
+        let groups;
+        it(`Given a restaurant with ${testCase.numGroups} tables with ${testCase.groupSize} seats each`, () => {
+          restaurant = createRestaurantWithTables(testCase.numGroups, testCase.groupSize);
+        });
+        it(`And ${testCase.numGroups} groups of ${testCase.groupSize}`, () => {
+          groups = createGroups(testCase.numGroups, testCase.groupSize);
+        });
+        it(`When all groups arrive and request tables`, () => {
+          groups.forEach((group) => restaurant.arrives(group));
+        });
+        it(`Then all groups should be allocated on a table`, () => {
+          groups.forEach((group) => {
+            const locatedTable = restaurant.locate(group);
+            expect(locatedTable).not.equal(null);
+          });
+        });
       });
-      it(`When all groups arrive and request tables`, () => {
-        groups.forEach((group) => restaurant.arrives(group));
-      });
-      it(`Then all groups should be allocated on a table`, () => {
-        groups.forEach((group) => {
-          const locatedTable = restaurant.locate(group);
-          expect(locatedTable).not.equal(null);
+    });
+
+    describe(`Allocate waiting groups on freed tables`, () => {
+      describe(`${testCase.numGroups} groups`, () => {
+        let restaurant;
+        let groups;
+        let waitingGroups;
+        it(`Given a restaurant with ${testCase.numGroups} table with ${testCase.groupSize} seats`, () => {
+          restaurant = createRestaurantWithTables(testCase.numGroups, testCase.groupSize + 1);
+        });
+        it(`And ${testCase.numGroups} groups of ${testCase.groupSize} people allocated`, () => {
+          groups = createGroups(testCase.numGroups, testCase.groupSize);
+          groups.forEach((group) => restaurant.arrives(group));
+        });
+        it(`And ${testCase.numGroups} waiting groups of ${testCase.groupSize + 1} people`, () => {
+          waitingGroups = createGroups(testCase.numGroups, testCase.groupSize);
+        });
+
+        it(`When all waiting groups arrive and request tables`, () => {
+          waitingGroups.forEach((group) => restaurant.arrives(group));
+        });
+        it(`Then waiting groups should be allocated on a table as soon as it gets freed `, () => {
+          for (let i = 0; i < testCase.numGroups; i++) {
+            restaurant.leave(groups[i]);
+          }
+          waitingGroups.forEach((group) => {
+            const locatedTable = restaurant.locate(group);
+            expect(locatedTable).not.equal(null);
+          });
         });
       });
     });
   });
 });
+
+function createRestaurantWithTables(numTables, tableSize) {
+  const tables = Array.from({ length: numTables }, () => new Table(tableSize));
+  return new Restaurant(tables);
+}
+
+function createGroups(numGroups, groupSize) {
+  return Array.from({ length: numGroups }, () => new CustomerGroup(groupSize));
+}
